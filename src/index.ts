f6a1e5dd6a34d68fb66f1547e3b3201e362daa46
shady-cj <petersp2000@gmail.com>
2025-02-17 18:09:07 +0100

    introducing swagger

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,32 @@ import { PrismaClient } from "@prisma/client";
 import "./redis-init";
 import { rootRoutes } from "./routes/root";
 import { CustomErrorMiddleware, InternalErrorMiddleware } from "./middlewares/errors";
+import swaggerJSDoc from "swagger-jsdoc";
+import { API_PORT } from "./secrets";
+
+// swagger ui initializations 
+const options = {
+    failOnErrors: true, // Whether or not to throw when parsing errors. Defaults to false.
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Quicrefill rest api',
+        version: '1.0.0',
+      },
+      servers: [
+        {
+            api: "http://localhost"
+        }
+      ]
+    },
+    apis: ['./routes*.ts'],
+  };
+  
+const openapiSpecification = swaggerJSDoc(options);
+
 const app = express();
 app.use(express.json())
+
 
 
 export const prismaClient = new PrismaClient()
@@ -15,4 +39,4 @@ app.use(rootRoutes)
 app.use(CustomErrorMiddleware)
 app.use(InternalErrorMiddleware)
 
-app.listen(4000, ()=>console.log('working'))
+app.listen(API_PORT, ()=>console.log('working'))
--- src/secrets.ts
+++ src/secrets.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 
+export const API_PORT = process.env.API_PORT
+export const API_HOST = process.env.API_HOST || "http:"
 export const DATABASE_URL = process.env.DATABASE_URL
 export const REDIS_HOST = process.env.REDIS_HOST
 export const REDIS_PORT = +process.env.REDIS_PORT!
